106b7168ab56451f541df9a7207116e5ce8eec34
Raswanth <raswanthvijai@gmail.com>
2022-09-28 12:31:18 +0530

    using @slf4j instead of creating log object explicitly

--- src/main/java/com/example/school/service/ContactService.java
+++ src/main/java/com/example/school/service/ContactService.java
@@ -1,13 +1,12 @@
 package com.example.school.service;
 
 import com.example.school.model.Contact;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class ContactService {
-    private static Logger log = LoggerFactory.getLogger(ContactService.class);
 
     public boolean saveMessageDetails(Contact contact){
         boolean isSaved = true;
